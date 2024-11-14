c32cac6837b79b5fe3d9d19abda6ca88c4cc5b21
Tejas <tejas.philipthomas@kalvium.community>
2024-11-14 09:29:36 +0530

    kiesha did this

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import data from "./resources/countryData.json";
-import "./App.css"
+import "./App.css";
 
 export default function App() {
   const [value, setValue] = useState("");
@@ -13,23 +13,36 @@ export default function App() {
     setValue(searchTerm);
   };
 
-  const handleKey = (e)=>{
-    if(e.key==="Escape"){
-      console.log(e.key)
+  const handleKey = (e) => {
+    if (e.key === "Escape") {
+      console.log(e.key);
       document.getElementById("dropdown").style.display = "none";
-    }
-    else{
+    } else {
       document.getElementById("dropdown").style.display = "inline";
     }
-  }
+  };
 
   return (
     <div className="App">
-      <h1>Search</h1>
+      <h1
+        style={{
+          color: "red",
+          backgroundColor: "lavenderblush",
+          padding: "10px",
+          fontFamily: "cursive",
+        }}
+      >
+        Search
+      </h1>
 
       <div>
         <div>
-          <input type="text" value={value} onChange={onChange} onKeyDown={handleKey} />
+          <input
+            type="text"
+            value={value}
+            onChange={onChange}
+            onKeyDown={handleKey}
+          />
           <button onClick={() => onSearch(value)}> Search </button>
         </div>
         <div id="dropdown">
@@ -46,10 +59,7 @@ export default function App() {
             })
             .slice(0, 10)
             .map((item) => (
-              <div
-                onClick={() => onSearch(item.name)}
-                key={item.name}
-              >
+              <div onClick={() => onSearch(item.name)} key={item.name}>
                 {item.name}
               </div>
             ))}
